886aebb709fe261e5c2975c0e02ab79a9da9b325
Khantdotcom <kokhanthtay@gmail.com>
2026-07-12 11:47:30 +0700

    Added C01 04-07

--- c01/ft_strlen.c
+++ c01/ft_strlen.c
@@ -1,18 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: khant.h <khant.h@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/12 11:21:41 by khant.h           #+#    #+#             */
-/*   Updated: 2026/07/12 11:21:42 by khant.h          ###   ########.fr       */
+/*   Created: 2026/07/12 11:35:51 by khant.h           #+#    #+#             */
+/*   Updated: 2026/07/12 11:45:04 by khant.h          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_div_mod(int a, int b, int *div, int *mod){
-    *div = a/b;
-    *mod = a % b;
+int     ft_strlen(char *str){
+    int count = 0;
+    while (*str != '\0'){
+        count += 1;
+        str++;
+    }
+    return (count);
+}
+
+int     main(void){
+    char c[] = "I love Pann";
+    int count = ft_strlen(c);
+    char cc = count + '0';
+    write(1,&cc,1);
 }
--- c01/ft_ultimate_div_mod.c
+++ c01/ft_ultimate_div_mod.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: khant.h <khant.h@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/12 11:21:41 by khant.h           #+#    #+#             */
-/*   Updated: 2026/07/12 11:21:42 by khant.h          ###   ########.fr       */
+/*   Created: 2026/07/12 11:21:18 by khant.h           #+#    #+#             */
+/*   Updated: 2026/07/12 11:21:20 by khant.h          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_div_mod(int a, int b, int *div, int *mod){
-    *div = a/b;
-    *mod = a % b;
+void    ft_ultimate_div_mod(int *a, int *b){
+    *a /= *b;
+}
+
+int     main(void){
+    int a = 2;
+    int b = 2;
+    ft_ultimate_div_mod(&a,&b);
+    char c = a + '0';
+    write(1,&c,1);
 }

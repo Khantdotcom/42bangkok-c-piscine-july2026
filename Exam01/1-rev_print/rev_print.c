360367d737772ae9b6456ecc9c9a2547eda47213
Khantdotcom <kokhanthtay@gmail.com>
2026-07-16 15:06:10 +0700

    Done : first_word in *args[ ]

--- Exam01/1-rev_print/rev_print.c
+++ Exam01/1-rev_print/rev_print.c
@@ -5,19 +5,18 @@
 //we must declare the array size or string size before use or no? if it's unknown what do we do?
 char    *ft_rev_print (char *str)
 {
-    int     i;
-    i = 0;
-    while(str[i] && str[i] != '\0')
-    {
-        i++;
-    }
-
-    while(i > 0)
-    {
-        write(1,&str[i],1);
-        i--;
-    }
     
+    
+}
+
+void	ft_swap(int *a, int *b)
+{
+    int temp;
+
+    temp = &a;
+    b = &temp;
+    a = &b;
+    //what is the different useages of pointers, references based on data types
 }
 
 //how to get char from command line
---
+++ Exam01/2-first_word/first_word.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+int main(int j, char *argv[])
+{
+    int     i;
+    
+    i = 0;
+    char *s = argv[1];
+
+
+    while(s[i] && s[i] != '\0' && j == 2)
+    {
+        if (s[i] != ' ')
+        {
+            write(1,&s[i],1);
+            if(s[i+1] == ' ')
+            {
+                break;
+            }
+        }
+        i++;
+    }
+    // while (&s[i] && &s[i] != '\0')
+    // {
+    //     i++;
+    // }
+
+    // while(i > 0)
+    // {
+    //     if (*c[i] != '\0')
+    //     {
+    //         write(1,&c[i],1);
+    //     }
+    // }
+}
